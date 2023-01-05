87a85b742b0643efe72a499c667134658e7ab151
jun315 <kouzus@naver.com>
2023-01-05 16:40:11 +0900

    m

--- src/Modifier.java
+++ src/Modifier.java
@@ -1,5 +1,5 @@
 public class Modifier {
     void accessModifier(){
-        System.out.println("Access Modifier : private,public,default,protected");
+        System.out.println("Access Modifier : private,public,default,protected"); 
     }
 }
