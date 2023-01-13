cd1fe0b60d30494cb190228905c5052a65d0441b
jun315 <kouzus@naver.com>
2023-01-13 15:13:57 +0900

    0113

--- src/aaa/Single.java
+++ src/aaa/Single.java
@@ -1,2 +1,12 @@
-package aaa;public class Single {
+package aaa;
+
+public class Single {//Eager Initialization 방식으로 해당 인스턴스를 사용하지 않더라도 인스턴스를 생성하기에 낭비가 될 수 있다.
+    private static final Single instance = new Single();
+    private Single(){
+        //생성자는 외부에서 호출하지 못하도록 private로 지정한다.
+    }
+    public static Single getInstance(){
+        return instance;
+    }
+
 }
--- src/aaa/Single2.java
+++ src/aaa/Single2.java
@@ -1,2 +1,15 @@
-package aaa;public class Single2 {
+package aaa;
+
+public class Single2 { //Lazy Initialization : 단일 스레드시 사용 , 효율이 좋음
+    private static Single2 instance;
+
+    private Single2(){}
+
+    public static Single2 getInstance(){
+        if(instance == null){
+            instance = new Single2();
+        }
+        return instance;
+    }
+
 }
