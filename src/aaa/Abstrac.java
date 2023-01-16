82187aba5783946093ba3946fc23d10d1306ec0f
jun315 <kouzus@naver.com>
2023-01-16 15:16:43 +0900

    0116

---
+++ src/aaa/Abstrac.java
@@ -0,0 +1,10 @@
+package aaa;
+
+abstract class Abstrac {
+    //추상메소드란 자식 클래스에서 반드시 오버라이딩해야 사용할 수 있음
+    public abstract int b();
+
+    public void d(){
+        System.out.println("d");
+    }
+}
